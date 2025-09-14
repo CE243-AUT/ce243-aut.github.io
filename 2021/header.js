bf4df8cc77df22bcb585db4a9cd29c99a9cfe2b1
rezaAdinepour <r3zaadinep0ur@gmail.com>
2025-09-14 12:05:02 +0330

    update heders

--- 2021/header.js
+++ 2021/header.js
@@ -29,8 +29,8 @@ document.write('\
   <a href="http://stanford.edu/">\
     <img src="images/stanfordlogo.png" class="logo-right">\
   </a>\
-  <h1>CE 243: Digital Electronics</h1>\
-  <h3>Amirkabir University of Technology (Tehran Polytechnic), Fall 2025</h3>\
+  <h1>CS 243: Machine Learning Systems Design</h1>\
+  <h3>Stanford, Winter 2021 [Archived]</h3>\
   <div style="clear:both;"></div>\
 </div>\
 ');
